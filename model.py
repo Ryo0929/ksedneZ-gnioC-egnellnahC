20c6f6558339bad5a5ac9727e5c9546d1b66159e
Weitung Liao <>
2021-11-28 21:55:06 -0700

    no message

--- model.py
+++ model.py
@@ -3,7 +3,8 @@ import requests
 import pandas as pd
 
 def get_data(domain,account,password):
-    response = requests.get(domain,auth=(account,password))
+    url='https://'+domain+'.zendesk.com/api/v2/tickets.json'
+    response = requests.get(url,auth=(account,password))
     if response.status_code==200:
         json_data = json.loads(response.text)
         if 'tickets'not in json_data.keys():
--- testing.py
+++ testing.py
@@ -9,10 +9,9 @@ def mocked_requests_get(*args, **kwargs):
             self.text = json_data
             self.status_code = status_code
 
-
-    if args[0] == 'http://zccryo.zendesk.com/tickets.json':
+    if args[0] == 'https://zccryo.zendesk.com/api/v2/tickets.json':
         return MockResponse('{"tickets":[{"key1": "value1"}]}', 200)
-    if args[0] == 'http://zccryo.zendesk.com/test.json':
+    if args[0] == 'https://others.zendesk.com/api/v2/tickets.json':
         return MockResponse('{"not_tickets":[{"key1": "value1"}]}', 200)
 
     return MockResponse(None, 404)
@@ -20,7 +19,7 @@ def mocked_requests_get(*args, **kwargs):
 @mock.patch('requests.get', side_effect=mocked_requests_get)
 class TestModel(unittest.TestCase):
     def test_fetch_data_success(self, mock_get):
-        correct_url='http://zccryo.zendesk.com/tickets.json'
+        correct_url='zccryo'
         result=get_data(correct_url,"acc","pwd")
         self.assertTrue(isinstance(result, pd.DataFrame))
 
@@ -30,7 +29,7 @@ class TestModel(unittest.TestCase):
         self.assertTrue(result is None)
 
     def test_fetch_data_invalid(self, mock_get):
-        different_api_call="http://zccryo.zendesk.com/test.json"
+        different_api_call="others"
         result=get_data(different_api_call,"acc","pwd")
         self.assertTrue(result is None)
 
